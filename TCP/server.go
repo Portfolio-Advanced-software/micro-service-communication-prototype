e530fe12543f30790074c0947b02fb2b09e597c4
Milofow <Milofow@hotmail.nl>
2023-05-17 10:43:09 +0200

    Changed tcp prototype

--- TCP/server.go
+++ TCP/server.go
@@ -3,38 +3,33 @@ package main
 import (
 	"fmt"
 	"net"
-
-	"github.com/Portfolio-Advanced-software/micro-service-communication-prototype/TCP/protocol"
 )
-
-func handleConnection(conn net.Conn) {
-	message, err := protocol.ReadMessage(conn)
-	if err != nil {
-		fmt.Println("Failed to read message:", err)
-		return
-	}
-
-	fmt.Println("Received message ID:", message.ID)
-	fmt.Println("Received message payload:", string(message.Payload))
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("Failed to start server:", err)
+		fmt.Println("Error listening:", err.Error())
 		return
 	}
 	defer listener.Close()
 
-	fmt.Println("Server started, waiting for connections...")
+	fmt.Println("Server listening on :8080")
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Failed to accept connection:", err)
-			continue
+			fmt.Println("Error accepting connection:", err.Error())
+			return
 		}
 
-		go handleConnection(conn)
+		go handleRequest(conn)
 	}
+}
+
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+
+	response := "Hello, client! This is the server response."
+
+	conn.Write([]byte(response))
 }
